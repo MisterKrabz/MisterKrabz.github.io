513e8971c2a1711adad7694fc21b195a39408742
PatrickBot9000 <pwangsuper@gmail.com>
2025-05-29 16:10:05 -0500

    first commit

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,12 +1,22 @@
 import React from 'react'
 import './navbar.css'
+import logo from '../assets/logo.png'
+import {Link} from 'react-scroll';
 
 const Navbar = () => {
     return(
         <nav>
-            <img src = "" al= ""/>
-            <div className = "desktopMenu"></div>
-            
+            <img src = {logo} alt= "Logo" className = "logo"/>
+            <div className = "desktopMenu">
+                <Link className = "desktopMenuListItem">Home</Link>
+                <Link className = "desktopMenuListItem">About</Link>
+                <Link className = "desktopMenuListItem">Skills</Link>
+                <Link className = "desktopMenuListItem">Projects</Link>
+                <Link className = "desktopMenuListItem">Contact</Link>
+            </div>
+            <button className = "desktopMenuButton">
+                <img src = "" alt = "menu icon"/>Contact Me!
+            </button>
         </nav>
     )
 }
