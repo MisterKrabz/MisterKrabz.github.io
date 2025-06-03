f41a0b54744cc76ed9f024bf56fad121f6d63112
PatrickBot9000 <pwangsuper@gmail.com>
2025-06-03 14:34:03 -0500

    there is a bug in contact.js file, browser says too many rerenders

--- src/components/Contact/contact.js
+++ src/components/Contact/contact.js
@@ -1,31 +1,36 @@
 import React from "react";  
+import {useState} from "react";
 import "./contact.css";
 
 const Contact = () => {
+    const [name1, setName1] = useState("");
+    const [email, setEmail] = useState("");
+    const [message, setMessage] = useState("");
+
 
     return(
-        <section class="contact-section">
-            <div class="contact-text-content">
-                <h1 class="main-heading">Excited<br/>to know<br/>more?</h1>
-                <h2 class="sub-heading">Let's talk!</h2>
+        <section className="contact-section">
+            <div className="contact-text-content">
+                <h1 className="main-heading">Excited<br/>to know<br/>more?</h1>
+                <h2 className="sub-heading">Let's talk!</h2>
             </div>
 
-            <div class="contact-form-container">
+            <div className="contact-form-container">
                 <form action="#" method="POST"> {/* Replace # with your form endpoint */}
-                    <div class="form-group">
+                    <div className="form-group">
                         <label htmlFor="name">Name</label>
-                        <input type="text" id="name" name="name" placeholder="Name" value = {name1}/>
+                        <input type="text" id="name" name="name" placeholder="Name" value = {name1} onChange = {(event) => setName1(event.target.value)}/>
                     </div>
-                    <div class="form-group">
+                    <div className="form-group">
                         <label htmlFor="email">Email</label>
-                        <input type="email" id="email" name="email" placeholder="Email Address" value = {email}/>
+                        <input type="email" id="email" name="email" placeholder="Email Address" value = {email} onChange = {(event) => setEmail(event.target.value)}/>
                     </div>
-                    <div class="form-group">
+                    <div className="form-group">
                         <label htmlFor="message">Message</label>
-                        <textarea id="message" name="message" rows="6" placeholder="Message" value = {message}></textarea>
+                        <textarea id="message" name="message" rows="6" placeholder="Message" value = {message} onChange = {(event) => setMessage(event.target.value)}></textarea>
                     </div>
-                    <div class="form-button-group">
-                        <button type="submit" disabled = {!name1 || !email || !message}class="send-button" onclick = {sendEmail}>Send</button>
+                    <div className="form-button-group">
+                        <button type="submit" disabled = {!name1 || !email || !message} className="send-button" onClick = {sendEmail}>Send</button>
                     </div>
                 </form>
             </div>
